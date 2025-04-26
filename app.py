30b3b1733c1310ff156c3be5b00bd4cb477ff04f
unknown <ashmitthawait2@gmail.com>
2025-04-27 03:28:50 +0530

    api setup

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from gemini import generate_manim_code
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return "hehe"
+    return {"message": "API working!"}
+
+
+@app.get("/code/{topic}")
+def get_code(topic: str):
+    return generate_manim_code(topic)
--- gemini.py
+++ gemini.py
@@ -10,8 +10,8 @@ client = genai.Client(api_key=GEMINI_KEY)
 
 
 def generate_manim_code(topic):
-    prompt = f"Manim is a python library used to generate mathematical/scientific/programming animations. You need to write python code using python's Manim library, to generate beautiful and aesthetic animations about a given code. Your response should strictly ONLY CONTAIN THE PYTHON CODE and nothing else, no intro or outro lines or any descriptions. The topic for which you need to write manim code is - {topic}"
+    prompt = f"Manim is a python library used to generate mathematical/scientific/programming animations. You need to write python code using python's Manim library, to generate beautiful and aesthetic animations about a given code. Your response should strictly ONLY CONTAIN THE PYTHON CODE and nothing else, no intro or outro lines or any descriptions. The output should be purely text, do not use code block formats (like ```python```). Do not write any comments in the code and use proper indentation to make sure it works correctly. The topic for which you need to write manim code is - {topic}"
     res = client.models.generate_content(
         model="gemini-2.0-flash", contents=prompt)
 
-    return res
+    return res.text
