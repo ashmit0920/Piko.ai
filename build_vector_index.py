de9de10e9e3002d914207e2678125bcfde8584e0
unknown <ashmitthawait2@gmail.com>
2025-06-09 01:17:00 +0530

    vector index built for docs (in conda env)

--- build_vector_index.py
+++ build_vector_index.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 DOCS_DIR = "manim_docs"
