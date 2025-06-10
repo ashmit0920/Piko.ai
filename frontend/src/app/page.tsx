4e6906e5c5cf8892b7675db4ff08e0376c60fbb8
unknown <ashmitthawait2@gmail.com>
2025-06-10 20:46:08 +0530

    frontend initialised

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import GradientText from '../components/ui/GradientText/GradientText';
+import { Button } from '@/components/ui/button';
+import shortid from "shortid";
+
+function getRandomKey() {
+  return shortid.generate();
+}
 
 export default function Home() {
   return (
@@ -8,17 +15,24 @@ export default function Home() {
         <h1 className="text-5xl font-bold bg-gradient-to-r from-cyan-600 via-blue-400 to-green-600 inline-block text-transparent bg-clip-text mb-4">
           Piko.ai
         </h1>
+        <GradientText
+          key={getRandomKey()}
+          colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+          animationSpeed={3}
+          showBorder={false}
+          className="text-5xl font-bold mb-4"
+        >
+          Add a splash of color!
+        </GradientText>
         <h1 className="text-2xl font-bold text-gray-300 mb-8">
           Your friendly neighbourhood learning assistant
         </h1>
         <p className="text-xl text-gray-500 mb-8">
           Generate beautiful Manim animations using AI. Just enter a topic, and get ready-to-run Manim code instantly.
         </p>
-        <Link href="/app">
-          {/* <a className="inline-block px-6 py-3 text-white bg-blue-600 hover:bg-blue-700 rounded-xl text-lg shadow-md transition">
-            Get Started
-          </a> */}
-        </Link>
+        <Button variant="outline">
+          Get Started
+        </Button>
       </section>
     </main>
   );
