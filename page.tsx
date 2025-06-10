73d2f0f240373b6002a8d05e7071b9b25254a7c0
unknown <ashmitthawait2@gmail.com>
2025-06-10 22:12:16 +0530

    nextjs and tailwind setup

--- page.tsx
+++ page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import GradientText from '../components/ui/GradientText/GradientText';
 import { Button } from '@/components/ui/button';
+import shortid from "shortid";
 import dynamic from 'next/dynamic';
+
+// const GradientText = dynamic(() => import('../components/ui/GradientText/GradientText.js'), {
+//   ssr: false, // This is crucial for client-side only components
+// });
+
+function getRandomKey() {
+  return shortid.generate();
+}
 
 export default function Home() {
   return (
@@ -10,16 +19,15 @@ export default function Home() {
         <h1 className="text-5xl font-bold bg-gradient-to-r from-cyan-600 via-blue-400 to-green-600 inline-block text-transparent bg-clip-text mb-4">
           Piko.ai
         </h1>
-
-        {/* <GradientText
+        <GradientText
+          key={getRandomKey()}
           colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
           animationSpeed={3}
           showBorder={false}
           className="text-5xl font-bold mb-4"
         >
           Add a splash of color!
-        </GradientText> */}
-        
+        </GradientText>
         <h1 className="text-2xl font-bold text-gray-300 mb-8">
           Your friendly neighbourhood learning assistant
         </h1>
